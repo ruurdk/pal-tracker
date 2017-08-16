6f7ff485054ba546e90ae3f8e03c6f667476fffb
Ruurd Keizer <ruurdk@hotmail.com>
2017-08-16 14:56:45 +0200

    spring actuator

--- src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
@@ -1,28 +1,28 @@
 package test.pivotal.pal.trackerapi;
 
-        import com.jayway.jsonpath.DocumentContext;
-        import io.pivotal.pal.tracker.PalTrackerApplication;
-        import io.pivotal.pal.tracker.TimeEntry;
-        import org.junit.Before;
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
-        import org.mariadb.jdbc.MariaDbDataSource;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.boot.test.context.SpringBootTest;
-        import org.springframework.boot.test.web.client.TestRestTemplate;
-        import org.springframework.http.HttpEntity;
-        import org.springframework.http.HttpMethod;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.jdbc.core.JdbcTemplate;
-        import org.springframework.test.context.junit4.SpringRunner;
+import com.jayway.jsonpath.DocumentContext;
+import io.pivotal.pal.tracker.PalTrackerApplication;
+import io.pivotal.pal.tracker.TimeEntry;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mariadb.jdbc.MariaDbDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-        import javax.sql.DataSource;
-        import java.util.Collection;
+import javax.sql.DataSource;
+import java.util.Collection;
 
-        import static com.jayway.jsonpath.JsonPath.parse;
-        import static org.assertj.core.api.Assertions.assertThat;
-        import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static com.jayway.jsonpath.JsonPath.parse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = RANDOM_PORT)
